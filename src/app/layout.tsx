9faf50a9cf2cc0a19706c6ca8b69f8899516286d
Rafal Labunski <rafallabunski@gmail.com>
2026-04-16 17:29:14 +0200

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { ReactNode } from "react";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -7,7 +12,7 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="es" className="h-full antialiased">
+    <html lang="es" className={cn("h-full antialiased", "font-sans", geist.variable)}>
       <body className="min-h-full">{children}</body>
     </html>
   );
